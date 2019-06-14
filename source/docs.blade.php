c456da7db694be8925e2585485f813d8e5eedb73
levonium <levon.avetyan@gmail.com>
2019-06-14 16:15:08 +0200

    Change Index Pages

--- source/docs.blade.php
+++ source/docs.blade.php
@@ -9,32 +9,41 @@
 </section>
 
 <section class="container max-w-4xl mx-auto p-8">
-    <h2 class="text-3xl font-black mb-8">Documentations</h2>
+    <h3 class="text-3xl font-black mb-4">Documentations</h3>
 
-    <hr class="block my-8 border lg:hidden">
+    {{-- <hr class="block my-8 border lg:hidden"> --}}
 
-    <div class="flex flex-wrap items-center">
+    <div class="flex flex-wrap items-center -mx-4">
 
-        <h3 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12 mr-4" alt="Akurai Docs">
-            <a class="text-grey-dark" href="/docs/akurai">Akurai <br class="hidden md:block lg:hidden" />Docs</a>
-        </h3>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-stack.svg" class="h-18 w-8 mr-4" alt="Akurai Docs">
+                <a class="uppercase text-grey-dark font-light" href="/docs/akurai">Akurai <br class="hidden md:block lg:hidden" />Docs</a>
+            </div>
+        </h4>
 
-        <h3 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12 mr-4" alt="Sikika Docs">
-            <a class="text-grey-dark" href="/docs/sikika">Sikika <br class="hidden md:block lg:hidden" />Docs</a>
-        </h3>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-stack.svg" class="h-18 w-8 mr-4" alt="Sikika Docs">
+                <a class="uppercase text-grey-dark font-light" href="/docs/sikika">Sikika <br class="hidden md:block lg:hidden" />Docs</a>
+            </div>
+        </h4>
 
-        <h3 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12 mr-4" alt="Kemoku Docs">
-            <a class="text-grey-dark" href="/docs/kemoku">Kemoku <br class="hidden md:block lg:hidden" />Docs</a>
-        </h3>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-stack.svg" class="h-18 w-8 mr-4" alt="Kemoku Docs">
+                <a class="uppercase text-grey-dark font-light" href="/docs/kemoku">Kemoku <br class="hidden md:block lg:hidden" />Docs</a>
+            </div>
+        </h4>
 
-        <h3 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12 mr-4" alt="Nonaki Docs">
-            <a class="text-grey-dark" href="/docs/nonaki">Nonaki <br class="hidden md:block lg:hidden" />Docs</a>
-        </h3>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-stack.svg" class="h-18 w-8 mr-4" alt="Nonaki Docs">
+                <a class="uppercase text-grey-dark font-light" href="/docs/nonaki">Nonaki <br class="hidden md:block lg:hidden" />Docs</a>
+            </div>
+        </h4>
 
     </div>
 </section>
+
 @endsection
--- source/index.blade.php
+++ source/index.blade.php
@@ -9,84 +9,106 @@
 </section>
 
 <section class="container max-w-4xl mx-auto p-8">
-    <h3 class="text-3xl font-black mb-8">Documentations</h3>
+    <h3 class="text-3xl font-black mb-4">Documentations</h3>
 
-    <hr class="block my-8 border lg:hidden">
+    {{-- <hr class="block my-8 border lg:hidden"> --}}
 
-    <div class="flex flex-wrap items-center">
+    <div class="flex flex-wrap items-center -mx-4">
 
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12 mr-4" alt="Akurai Docs">
-            <a class="text-grey-dark font-light" href="/docs/akurai">Akurai <br class="hidden md:block lg:hidden" />Docs</a>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-stack.svg" class="h-18 w-8 mr-4" alt="Akurai Docs">
+                <a class="uppercase text-grey-dark font-light" href="/docs/akurai">Akurai <br class="hidden md:block lg:hidden" />Docs</a>
+            </div>
         </h4>
 
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12 mr-4" alt="Sikika Docs">
-            <a class="text-grey-dark font-light" href="/docs/sikika">Sikika <br class="hidden md:block lg:hidden" />Docs</a>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-stack.svg" class="h-18 w-8 mr-4" alt="Sikika Docs">
+                <a class="uppercase text-grey-dark font-light" href="/docs/sikika">Sikika <br class="hidden md:block lg:hidden" />Docs</a>
+            </div>
         </h4>
 
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12 mr-4" alt="Kemoku Docs">
-            <a class="text-grey-dark font-light" href="/docs/kemoku">Kemoku <br class="hidden md:block lg:hidden" />Docs</a>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-stack.svg" class="h-18 w-8 mr-4" alt="Kemoku Docs">
+                <a class="uppercase text-grey-dark font-light" href="/docs/kemoku">Kemoku <br class="hidden md:block lg:hidden" />Docs</a>
+            </div>
         </h4>
 
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12 mr-4" alt="Nonaki Docs">
-            <a class="text-grey-dark font-light" href="/docs/nonaki">Nonaki <br class="hidden md:block lg:hidden" />Docs</a>
-        </h4>
-
-    </div>
-</section>
-
-<section class="container max-w-4xl mx-auto p-8">
-    <h3 class="text-3xl font-black mb-8">Demos</h3>
-
-    <hr class="block my-8 border lg:hidden">
-
-    <div class="flex flex-wrap items-center">
-
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-window.svg" class="h-12 w-12 mr-4" alt="Akurai Demos">
-            <a class="text-grey-dark font-light" href="https://dinomatic.com/demos/akurai">Akurai <br class="hidden md:block lg:hidden" />Demos</a>
-        </h4>
-
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-window.svg" class="h-12 w-12 mr-4" alt="Sikika Demos">
-            <a class="text-grey-dark font-light" href="https://dinomatic.com/demos/sikika">Sikika <br class="hidden md:block lg:hidden" />Demos</a>
-        </h4>
-
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-window.svg" class="h-12 w-12 mr-4" alt="Kemoku Demos">
-            <a class="text-grey-dark font-light" href="https://dinomatic.com/demos/kemoku">Kemoku <br class="hidden md:block lg:hidden" />Demos</a>
-        </h4>
-
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-window.svg" class="h-12 w-12 mr-4" alt="Nonaki Demos">
-            <a class="text-grey-dark font-light" href="https://dinomatic.com/demos/nonaki">Nonaki <br class="hidden md:block lg:hidden" />Demos</a>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-stack.svg" class="h-18 w-8 mr-4" alt="Nonaki Docs">
+                <a class="uppercase text-grey-dark font-light" href="/docs/nonaki">Nonaki <br class="hidden md:block lg:hidden" />Docs</a>
+            </div>
         </h4>
 
     </div>
 </section>
 
 <section class="container max-w-4xl mx-auto p-8">
-    <h3 class="text-3xl font-black mb-8">Support</h3>
+    <h3 class="text-3xl font-black mb-4">Demos</h3>
 
-    <hr class="block my-8 border lg:hidden">
+    {{-- <hr class="block my-8 border lg:hidden"> --}}
 
-    <div class="flex flex-wrap items-center">
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-terminal.svg" class="h-12 w-12 mr-4" alt="Support">
-            <a class="text-grey-dark font-light" href="https://dinomatic.com/account">Get <br class="hidden md:block lg:hidden" />Support</a>
+    <div class="flex flex-wrap items-center -mx-4">
+
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-window.svg" class="h-8 w-8 mr-4" alt="Akurai Demos">
+                <a class="uppercase text-grey-dark font-light" href="https://dinomatic.com/demos/akurai">Akurai <br class="hidden md:block lg:hidden" />Demos</a>
+            </div>
         </h4>
 
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-terminal.svg" class="h-12 w-12 mr-4" alt="Blog Posts">
-            <a class="text-grey-dark font-light" href="https://dinomatic.com/posts">Blog <br class="hidden md:block lg:hidden" />Posts</a>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-window.svg" class="h-8 w-8 mr-4" alt="Sikika Demos">
+                <a class="uppercase text-grey-dark font-light" href="https://dinomatic.com/demos/sikika">Sikika <br class="hidden md:block lg:hidden" />Demos</a>
+            </div>
         </h4>
 
-        <h4 class="text-2xl md:text-xl lg:text-2xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
-            <img src="/assets/img/icon-terminal.svg" class="h-12 w-12 mr-4" alt="WordPress Hosting">
-            <a class="text-grey-dark font-light" href="https://dinomatic.com/posts/wordpress-hosting">WordPress <br class="hidden md:block lg:hidden" />Hosting</a>
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-window.svg" class="h-8 w-8 mr-4" alt="Kemoku Demos">
+                <a class="uppercase text-grey-dark font-light" href="https://dinomatic.com/demos/kemoku">Kemoku <br class="hidden md:block lg:hidden" />Demos</a>
+            </div>
+        </h4>
+
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-window.svg" class="h-8 w-8 mr-4" alt="Nonaki Demos">
+                <a class="uppercase text-grey-dark font-light" href="https://dinomatic.com/demos/nonaki">Nonaki <br class="hidden md:block lg:hidden" />Demos</a>
+            </div>
+        </h4>
+
+    </div>
+</section>
+
+<section class="container max-w-4xl mx-auto p-8">
+    <h3 class="text-3xl font-black mb-4">Support</h3>
+
+    {{-- <hr class="block my-8 border lg:hidden"> --}}
+
+    <div class="flex flex-wrap items-center -mx-4">
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-terminal.svg" class="h-8 w-8 mr-4" alt="Support">
+                <a class="uppercase text-grey-dark font-light" href="https://dinomatic.com/account">Get <br class="hidden md:block lg:hidden" />Support</a>
+            </div>
+        </h4>
+
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-terminal.svg" class="h-8 w-8 mr-4" alt="Blog Posts">
+                <a class="uppercase text-grey-dark font-light" href="https://dinomatic.com/posts">Blog <br class="hidden md:block lg:hidden" />Posts</a>
+            </div>
+        </h4>
+
+        <h4 class="px-4 text-xl text-blue-darkest mb-8 mt-0 flex items-center w-full sm:w-1/2 md:w-1/4">
+            <div class="w-full p-4 flex items-center border-b border-teal">
+                <img src="/assets/img/icon-terminal.svg" class="h-8 w-8 mr-4" alt="WordPress Hosting">
+                <a class="uppercase text-grey-dark font-light" href="https://dinomatic.com/posts/wordpress-hosting">WordPress <br class="hidden md:block lg:hidden" />Hosting</a>
+            </div>
         </h4>
 
     </div>
